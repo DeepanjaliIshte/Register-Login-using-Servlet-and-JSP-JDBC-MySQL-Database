a4f2de217a4d15f17126f883cbc68917e2935bff
Deepanjali Ishte <159618789+DeepanjaliIshte@users.noreply.github.com>
2024-06-13 23:38:24 +0530

    Add files via upload

---
+++ reg_demo.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE reg_demo;
+USE reg_demo;
+CREATE TABLE register(name VARCHAR(100),email VARCHAR(50),password VARCHAR(20),gender VARCHAR(10),city VARCHAR(20));
+
+select *from register;
